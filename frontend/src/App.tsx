8ba67f4ce000239071a08ef67e5aefc1ffbabe5e
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-05-13 21:08:06 +0900

    update App.tsx to use WebRTC

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,19 +5,23 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [socket, setSocket] = useState<Socket | null>(null);
+  const [connection, setConnection] = useState<RTCPeerConnection | null>(null);
   useEffect(() => {
     const socket = io('http://localhost:3000');
+    const connection = new RTCPeerConnection({ iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] });
+    setConnection(connection);
     setSocket(socket);
     return () => {
       socket.disconnect();
+      connection.close();
     };
   }, []);
-  if (socket === null) return <></>;
+  if (!socket || !connection) return <></>;
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home socket={socket} />} />
+          <Route path="/" element={<Home socket={socket} connection={connection} />} />
         </Routes>
       </BrowserRouter>
     </>
